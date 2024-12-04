b3a6c1c6fb814ac2c4395dfb554b41968a4b5d7b
rahullohar75 <rahulohar75@gmail.com>
2024-12-04 21:23:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || '👑𝐑𝐀𝐇𝐔𝐋-𝐏𝐀𝐍𝐂𝐇𝐀𝐋👑';
 global.pairingNumber = process.env.BOT_NUMBER || '917017659124';  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '6VZAwLZK#SkgkCMWrwsVFNvhPq3HH4RAYPkQpg380QSvNTi52N40';  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID || 'jZBEgQ5C#oK-yt9EFLNxvQiN_bun5hJVUth_3twxcDnnSZTzOL2o';  // put your session id here
 
 global.mods = []
 global.prems = []
